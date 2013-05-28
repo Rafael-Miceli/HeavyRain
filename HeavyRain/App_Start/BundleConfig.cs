2310289121494c7b633af3128e1cb1f3427b4960
Rafael Miceli <rafael.miceli@hotmail.com>
2013-05-27 22:41:01 -0300

    Creating Images Carousel

--- HeavyRain/App_Start/BundleConfig.cs
+++ HeavyRain/App_Start/BundleConfig.cs
@@ -12,6 +12,17 @@ namespace HeavyRain
 
             bundles.Add(new ScriptBundle("~/javascript/bootstrap").Include(
                 "~/bootstrap/js/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/css/carousel").Include(
+                "~/Content/Carousel/css/Carousel.css"));
+
+            bundles.Add(new ScriptBundle("~/javascript/carousel").Include(
+                "~/Content/Carousel/javascript/Carousel.js",
+                "~/Content/Carousel/javascript/jquery.ba-throttle-debounce.min.js",
+                "~/Content/Carousel/javascript/jquery.carouFredSel-6.2.1-packed.js",
+                "~/Content/Carousel/javascript/jquery.mousewheel.min.js",
+                "~/Content/Carousel/javascript/jquery.touchSwipe.min.js",
+                "~/Content/Carousel/javascript/jquery.transit.min.js"));
         }
     }
 }
